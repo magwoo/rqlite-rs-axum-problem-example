ff749f0ad070201debee929c3e5d65936c2b7787
magwoo <magwood51@yandex.ru>
2025-02-19 19:19:07 +0300

    added problem-example code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::extract::State;
+use axum::routing::get;
+use axum::Router;
+use rqlite_rs::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", get(example_endpoint));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+
+    axum::serve(listener, router).await.unwrap()
+}
+
+async fn example_endpoint(State(db): State<RqliteClient>) -> &'static str {
+    ""
 }
